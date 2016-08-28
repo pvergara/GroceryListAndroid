61f984cf690e22d2ef0d3bb21fb0bf1b8bf964f3
Pablo Cristóbal Vergara Castro <pablocristobal@gmail.com>
2016-08-28 23:22:30 +0200

    * Add the behaviour to act when the sendItem was triggered   * Add the properties classes to make more ease to manage the binding between View and ViewModel   * Add the ability to unRegister the class to the MessagingService

--- app/src/main/java/org/ecos/groceryList/viewModels/ListCreationViewModel.java
+++ app/src/main/java/org/ecos/groceryList/viewModels/ListCreationViewModel.java
@@ -1,11 +1,21 @@
 package org.ecos.groceryList.viewModels;
 
+import org.ecos.android.infrastructure.mvvm.binding.OnChangeListener;
+import org.ecos.android.infrastructure.mvvm.binding.Property;
 import org.ecos.android.infrastructure.mvvm.viewModel.ViewModel;
+import org.ecos.groceryList.R;
 
 import java.util.ArrayList;
 
 public interface ListCreationViewModel extends ViewModel {
+    class Properties{
+        public static Property addItem = Property.from(R.id.listCreationGroceryList, "addItem");
+    }
+    void setOnchangeListener(OnChangeListener onChangeListener);
+
     void init();
 
     ArrayList<String> getCollection();
+
+    void deInit();
 }
--- app/src/main/java/org/ecos/groceryList/viewModels/ListCreationViewModelImpl.java
+++ app/src/main/java/org/ecos/groceryList/viewModels/ListCreationViewModelImpl.java
@@ -1,13 +1,34 @@
 package org.ecos.groceryList.viewModels;
 
+import org.ecos.android.infrastructure.messaging.MessagingService;
+import org.ecos.android.infrastructure.mvvm.binding.OnChangeListener;
+import org.ecos.groceryList.events.ItemSendEvent;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ListCreationViewModelImpl implements ListCreationViewModel {
+    private final MessagingService mMessagingService;
     private ArrayList<String> mCollection;
+    private OnChangeListener mOnChangeListener;
 
     public ArrayList<String> getCollection() {
         return mCollection;
+    }
+
+
+    @Override
+    public void setOnchangeListener(OnChangeListener onChangeListener) {
+        mOnChangeListener = onChangeListener;
+    }
+
+    public ListCreationViewModelImpl(MessagingService messagingService) {
+        mMessagingService = messagingService;
+
+        mMessagingService.registerMe(this);
+
     }
 
     @Override
@@ -22,6 +43,25 @@ public class ListCreationViewModelImpl implements ListCreationViewModel {
 
     private void initData() {
         mCollection.addAll(Arrays.asList("1","2","3","4","5","6","7","8","9","10","1","2","3","4","5","6","7","8","9","10"));
+    }
+
+    //TODO: ABSTRACTION (register inside Messaging Service)
+    @Subscribe(sticky = true, threadMode = ThreadMode.MAIN)
+    public void onEvent(ItemSendEvent event) {
+        addNewItemWith(event.getItemText());
+    }
+
+    private void addNewItemWith(CharSequence itemText) {
+        if(itemText!=null) {
+            mCollection.add(itemText.toString());
+            mOnChangeListener.onPropertyChange(Properties.addItem, itemText);
+        }
+    }
+
+
+    @Override
+    public void deInit(){
+        mMessagingService.unRegisterMe(this);
     }
 
 }
