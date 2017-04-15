cc0614e88d78ae5c7839321b19e6e358c68c500d
Pablo Cristóbal Vergara Castro <pablocristobal@gmail.com>
2017-04-15 02:06:51 +0200

    unnecessary code

--- infrastructure/src/main/java/org/ecos/mvvm/MyClass.java
+++
@@ -1,7 +0,0 @@
-package org.ecos.mvvm;
-
-interface NotificationHandler {
-    void notify(String string, Object value);
-
-    void manageOnChangeFor(String propertyName, BindingAction bindingAction, Activity activity);
-}
