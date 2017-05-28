2b56d0cc08b3d2b94c0bfe4cbcce072008eab852
Pablo Cristóbal Vergara Castro <pablocristobal@gmail.com>
2017-05-28 23:51:01 +0200

    * Cleaning unnecessary (and dangerous) code   * Renaming views   * Adding the availability to show list item layout inside the reclyclerview preview   * ANIMATIONS!!!

--- app/src/main/java/org/ecos/groceryList/views/ItemViewHolder.java
+++ app/src/main/java/org/ecos/groceryList/views/ItemViewHolder.java
@@ -1,5 +1,8 @@
 package org.ecos.groceryList.views;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
@@ -11,37 +14,60 @@ import org.ecos.groceryList.events.ItemSendToUpdateEvent;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 @SuppressWarnings("ALL")
-public class ItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-    private final MessagingService mMessagingService;
-    @BindView(R.id.textView)
-    TextView textView;
+public class ItemViewHolder extends RecyclerView.ViewHolder {
+    public static final String BACKGROUND_COLOR = "backgroundColor";
+    private MessagingService mMessagingService;
+
+    @BindView(R.id.item_as_text)
+    TextView mItemText;
+
+    @BindView(R.id.item_container)
+    View mItemContainer;
+
 
     private Item mItem;
 
-    public TextView getTextView() {
-        return textView;
+    public void bind(Item item) {
+        mItem = item;
+
+        updateViews();
     }
+
+    private void updateViews() {
+        mItemText.setText(mItem.getName().toString());
+    }
+
 
     public ItemViewHolder(View view, MessagingService messagingService) {
         super(view);
-        mMessagingService = messagingService;
-        ButterKnife.bind(this, view);
-
-        view.setOnClickListener(this);
+        initDependencies(view, messagingService);
     }
 
-    @Override
-    public void onClick(View view) {
+    private void initDependencies(View view, MessagingService messagingService) {
+        mMessagingService = messagingService;
+        ButterKnife.bind(this, view);
+    }
+
+    @OnClick(R.id.item_container)
+    public void onClick() {
+        showsSelectionAnimation(mItemContainer);
         sendBackToUpdate();
+    }
+
+    private void showsSelectionAnimation(View myView) {
+        ObjectAnimator fadeIn = ObjectAnimator.ofArgb(myView, BACKGROUND_COLOR, Color.argb(128, 170, 245, 208), Color.argb(0, 170, 245, 208));
+        fadeIn.setDuration(500);
+
+
+        final AnimatorSet mAnimationSet = new AnimatorSet();
+        mAnimationSet.play(fadeIn);
+        mAnimationSet.start();
     }
 
     private void sendBackToUpdate() {
         mMessagingService.send(new ItemSendToUpdateEvent(mItem));
-    }
-
-    public void bind(Item item) {
-        mItem = item;
     }
 }
