590de82047938567be9c189a4f5b42a034468f9b
Pablo Cristóbal Vergara Castro <pablocristobal@gmail.com>
2017-06-07 01:39:30 +0200

    GENERIC   * Playing with regexps!!!

--- core/src/main/java/org/ecos/grocerylist/core/service/ItemStringSplitter.java
+++ core/src/main/java/org/ecos/grocerylist/core/service/ItemStringSplitter.java
@@ -4,10 +4,19 @@ import org.ecos.grocerylist.core.items.ItemPart;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ItemStringSplitter {
     public static Map<ItemPart, String> split(String itemString) {
+        final Pattern p = Pattern.compile("^([a-zA-Z]+)\\s?\\+?([0-9]+)?");
         return new HashMap<ItemPart,String>(){{
-            put(ItemPart.name,itemString);}};
-    }
+            Matcher m = p.matcher(itemString);
+            if(m.matches()) {
+                put(ItemPart.name, m.group(1));
+                if (m.groupCount() == 2 && m.group(2) != null)
+                    put(ItemPart.quantity, m.group(2));
+            }
+        }};
+        }
 }
